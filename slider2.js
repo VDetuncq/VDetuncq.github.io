f15ee9913a92d70a801179ee265d0e2d969776f3
VDetuncq <96585535+VDetuncq@users.noreply.github.com>
2021-12-27 17:05:25 +0100

    Update slider2.js

--- slider2.js
+++ slider2.js
@@ -1,11 +1,11 @@
-var slide = new Array("abbeville0.jpg", "abbeville1.jpg", "abbeville2.jpg", "abbeville3.jpg", "abbeville4.jpg", "abbeville5.jpg", "abbeville6.jpg", "abbeville7.jpg");
+var slide2 = new Array("abbeville0.jpg", "abbeville1.jpg", "abbeville2.jpg", "abbeville3.jpg", "abbeville4.jpg", "abbeville5.jpg", "abbeville6.jpg", "abbeville7.jpg");
 var numero = 0;
 
-function ChangeSlide(sens) {
+function ChangeSlide2(sens) {
     numero = numero + sens;
     if (numero < 0)
         numero = slide.length - 1;
     if (numero > slide.length - 1)
         numero = 0;
-    document.getElementById("slide").src = slide[numero];
+    document.getElementById("slide2").src = slide[numero];
 }
