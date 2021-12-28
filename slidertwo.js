dbfe160ac4273aa5b8cff4c15ed6d4e6bf7690d9
VDetuncq <96585535+VDetuncq@users.noreply.github.com>
2021-12-28 18:26:12 +0100

    Update and rename slider2.js to slidertwo.js

---
+++ slidertwo.js
@@ -0,0 +1,11 @@
+var slidetwo = new Array("abbeville0.JPG", "abbeville1.JPG", "abbeville2.JPG", "abbeville3.JPG", "abbeville4.JPG", "abbeville5.JPG", "abbeville6.JPG", "abbeville7.JPG");
+var numero = 0;
+
+function ChangeSlidetwo(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slidetwo.length - 1;
+    if (numero > slidetwo.length - 1)
+        numero = 0;
+    document.getElementById("slide2").src = slidetwo[numero];
+}
